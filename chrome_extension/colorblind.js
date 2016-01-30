bfe9236f1af3092a2d2a77acbcb7bd91c4395a87
Salamon David <salamon.david@gmail.com>
2016-01-31 00:10:50 +0100

    Merge stripes with original color

--- chrome_extension/colorblind.js
+++ chrome_extension/colorblind.js
@@ -1,8 +1,8 @@
 // colorblind.js
 // 
 // Uses TinyColor library https://github.com/bgrins/TinyColor
-
-//=========================================================
+// 
+//===============================================
 // 
 // TODO: Integrate original color into pattern
 //       Useful reading:
@@ -10,6 +10,7 @@
 //        http://micheljansen.org/blog/entry/1238
 //        https://github.com/SachaG/Patternify
 //        http://www.motobit.com/util/base64-decoder-encoder.asp
+//        http://stackoverflow.com/questions/23190056/hex-to-base64-converter-for-javascript
 // TODO: Collect all relevant colours up front and only match in the iteration to increase speed(?)
 // TODO: Adjustable CSS Opacity? -> not gonna work with GIF
 // TODO: Support background color in :after and ::before pseudo-elements
@@ -20,7 +21,7 @@
 // TODO: bower package
 // TODO: Chrome Extension package
 // 
-//=========================================================
+//===============================================
 
 // Start execution on incoming message from Extension button
 chrome.runtime.onMessage.addListener(
@@ -31,9 +32,9 @@ chrome.runtime.onMessage.addListener(
   }
 );
 
-//=========================================================
+//===============================================
 
-var unBlindPage = function(message) {
+function unBlindPage(message) {
   
   // Iterate over all elements in page - 
   // fastest is to reverse iterate with plain JS
@@ -52,34 +53,59 @@ var unBlindPage = function(message) {
   }
 };
 
-//===============================================
+//=====================================
 
-var unBlindItem = function(item) {
+function unBlindItem(item) {
   // Get CSSStyleDeclaration object of item
   var color = window.getComputedStyle(item).backgroundColor;
   // Get cylindrical representation of color from RGB (en.wikipedia.org/wiki/HSL_and_HSV)
   var colorHSV = tinycolor(color).toHsv();
+  // Get components of color
+  var colorComponents = color.substring(color.indexOf('(') + 1, color.lastIndexOf(')')).split(/,\s*/)
   // Reference value of transparent background returned by getComputedStyle
   var transparent = 'rgba(0, 0, 0, 0)';
 
   // Modify item, unless the background color is transparent or low-chroma
   if ((color.localeCompare(transparent) !== 0) && (colorHSV.s > 0.1)) {
-    pattern = getPatternForColor(item,color);
+    pattern = getPatternForComponents(colorComponents);
     item.style.background = pattern;
   }
 };
 
-//===============================================
+//=====================================
 
-var getPatternForColor = function(item, color) {
+function getPatternForComponents(components) {
   
+  var originalColor = rgbToHex(components[0], components[1], components[2]);
   // 2x1 image in black/white for vertical stripes
-  var verticalStripes = 'R0lGODdhAgABAIAAAAAAAP///ywAAAAAAgABAAACAkQKADs='
-  
+  var verticalStripes = "47 49 46 38 37 61 02 00 01 00 80 00 00 00 00 00 " + originalColor + " 2c 00 00 00 00 02 00 01 00 00 02 02 44 0a 00 3b";
   // 1x2 image in black/white for horizontal stripes
-  var horizontalStripes = 'R0lGODdhAQACAIAAAAAAAP///ywAAAAAAQACAAACAkQKADs='
-  
-  var base64Pattern = "url(data:image/gif;base64," + verticalStripes + ") repeat";
+  var horizontalStripes = "47 49 46 38 37 61 01 00 02 00 80 00 00 00 00 00 " + originalColor + " 2c 00 00 00 00 01 00 02 00 00 02 02 44 0a 00 3b";
 
-  return base64Pattern;
+  if (true) {
+    return "url(data:image/gif;base64," + hexToBase64(horizontalStripes) + ") repeat";
+  } else {
+    return "url(data:image/gif;base64," + hexToBase64(verticalStripes) + ") repeat";
+  }
 };
+
+//=== CONVERSION UTILS ====================================
+
+function rgbToHex(r, g, b) {
+  return ((1 << 24) + (parseInt(r) << 16) + (parseInt(g) << 8) + parseInt(b)).toString(16).substr(1);
+}
+
+function hexToBase64(str) {
+  return btoa(String.fromCharCode.apply(null,
+    str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" "))
+  );
+}
+
+function base64ToHex(str) {
+  for (var i = 0, bin = atob(str.replace(/[ \r\n]+$/, "")), hex = []; i < bin.length; ++i) {
+    var tmp = bin.charCodeAt(i).toString(16);
+    if (tmp.length === 1) tmp = "0" + tmp;
+    hex[hex.length] = tmp;
+  }
+  return hex.join(" ");
+}
