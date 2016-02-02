30bd99c9a2bdd5f4cb22e2d56b769ada3f8ab688
Salamon David <salamon.david@gmail.com>
2016-02-02 22:46:10 +0100

    Font color support added

--- chrome_extension/zuckerberg.js
+++ chrome_extension/zuckerberg.js
@@ -25,18 +25,25 @@ function unBlindPage(message) {
 //===============================================
 
 function unBlindItem(item) {
-  // Get color from CSSStyleDeclaration of item
-  var color = tinycolor(window.getComputedStyle(item).backgroundColor);
-  // Get cylindrical representation of color from RGB (en.wikipedia.org/wiki/HSL_and_HSV)
-  var colorHSV = color.toHsv();
-  // Reference value of transparent background returned by getComputedStyle
-  var transparent = 'rgba(0, 0, 0, 0)';
-
-  // Modify item, unless the background color is transparent or low-chroma
-  if ((color.toString().localeCompare(transparent) !== 0) && (colorHSV.s > 0.1)) {
-    pattern = getPatternForColor(color);
+  // Get CSSStyleDeclaration of item
+  var itemCss = window.getComputedStyle(item);
+  // Get color objects from CSS
+  var bgColor = tinycolor(itemCss.backgroundColor);
+  var fontColor = tinycolor(itemCss.color);
+  
+  if (isColorful(fontColor)) {
+    
+    pattern = getPatternForColor(fontColor);
     item.style.background = pattern;
     item.style.backgroundSize = window.zBParams.stripeWidth + " " + window.zBParams.stripeWidth;
+    item.style.color = "#FFF";
+
+  } else if (isColorful(bgColor)) {
+    
+    pattern = getPatternForColor(bgColor);
+    item.style.background = pattern;
+    item.style.backgroundSize = window.zBParams.stripeWidth + " " + window.zBParams.stripeWidth;
+  
   }
 };
 
@@ -44,26 +51,21 @@ function unBlindItem(item) {
 
 function getPatternForColor(original) {
   var color = original.clone();
-  var _isred = false;
-  var _isgreen = false;
-
-  if ( ((_isred = isRed(original)) || (_isgreen = isGreen(original))) && !isBlue(original)) {
-    var darker = color.darken().toHexString().substr(1)
-    var lighter = color.brighten(30).saturate(50).toHexString().substr(1)
-    var pattern;
-    
-    if (_isred) {
-      // 1x2 image in black/white for horizontal stripes
-      pattern = "47 49 46 38 37 61 01 00 02 00 80 00 00" + lighter + darker + "2c 00 00 00 00 01 00 02 00 00 02 02 44 0a 00 3b";
-    } else {
-      // 2x1 image in black/white for vertical stripes
-      pattern = "47 49 46 38 37 61 02 00 01 00 80 00 00" + lighter + darker + "2c 00 00 00 00 02 00 01 00 00 02 02 44 0a 00 3b";
-    }
-    return "url(data:image/gif;base64," + hexToBase64(pattern) + ") repeat";
+  var darker = color.darken().toHexString().substr(1)
+  var lighter = color.brighten(30).saturate(50).toHexString().substr(1)
+  var pattern;
+  
+  if (isRed(original)) {
+    // 1x2 image in black/white for horizontal stripes
+    pattern = "47 49 46 38 37 61 01 00 02 00 80 00 00" + lighter + darker + "2c 00 00 00 00 01 00 02 00 00 02 02 44 0a 00 3b";
   } else {
-    return original.toString();
+    // 2x1 image in black/white for vertical stripes
+    pattern = "47 49 46 38 37 61 02 00 01 00 80 00 00" + lighter + darker + "2c 00 00 00 00 02 00 01 00 00 02 02 44 0a 00 3b";
   }
+  return "url(data:image/gif;base64," + hexToBase64(pattern) + ") repeat";
 };
+
+//=== COLOR UTILS =========================================
 
 function isRed(color) {
   var rgb = color.toRgb();
@@ -79,6 +81,13 @@ function isBlue(color) {
   var rgb = color.toRgb();
   var bt = window.zBParams.blueTreshold;
   return (rgb.r < rgb.b*0.8) && (rgb.g < rgb.b*0.8) && (rgb.b > bt*255)
+}
+
+function isColorful(color) {
+  // Get cylindrical representation of color from RGB (en.wikipedia.org/wiki/HSL_and_HSV)
+  var colorHSV = color.toHsv();
+  var _isSaturated = colorHSV.s > 0.1;
+  return _isSaturated && !isBlue(color) && ( isRed(color) || isGreen(color) );
 }
 
 //=== CONVERSION UTILS ====================================
